a7bc9511945fdbd2ff6124c32f98f6fb91c2b93a
Mujhtech <mujeeb.muhideen@gmail.com>
2021-10-07 13:53:14 +0100

    deploy contract on celo testnet

---
+++ contract/EvwpContract.sol
@@ -0,0 +1,164 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.7.0 <0.9.0;
+
+interface IERC20Token {
+  function transfer(address, uint256) external returns (bool);
+  function approve(address, uint256) external returns (bool);
+  function transferFrom(address, address, uint256) external returns (bool);
+  function totalSupply() external view returns (uint256);
+  function balanceOf(address) external view returns (uint256);
+  function allowance(address, address) external view returns (uint256);
+
+  event Transfer(address indexed from, address indexed to, uint256 value);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+
+library SafeMath {
+  /**
+  * @dev Multiplies two numbers, reverts on overflow.
+  */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+    // benefit is lost if 'b' is also tested.
+    // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
+    if (a == 0) {
+      return 0;
+    }
+
+    uint256 c = a * b;
+    require(c / a == b);
+    return c;
+
+  }
+
+  /**
+  * @dev Integer division of two numbers truncating the quotient, reverts on division by zero.
+  */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    require(b > 0); // Solidity only automatically asserts when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+
+  }
+
+  /**
+  * @dev Subtracts two numbers, reverts on overflow (i.e. if subtrahend is greater than minuend).
+  */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    require(b <= a);
+    uint256 c = a - b;
+    return c;
+  }
+
+  /**
+  * @dev Adds two numbers, reverts on overflow.
+  */
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    require(c >= a);
+    return c;
+  }
+
+  /**
+  * @dev Divides two numbers and returns the remainder (unsigned integer modulo),
+  * reverts when dividing by zero.
+  */
+  function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+    require(b != 0);
+    return a % b;
+  }
+
+}
+
+contract EvwpContract {
+    
+    using SafeMath for uint256; 
+
+    uint internal candidatesLength = 0;
+    address internal cUsdTokenAddress = 0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
+
+    struct Candidate {
+        string name;
+        string image;
+        string description;
+        string occupation;
+        uint vote;
+    }
+
+    mapping (uint => Candidate) internal candidates;
+    address public admin;
+    uint256 public availableFunds;
+
+    function writeCandidate(
+        string memory _name,
+        string memory _image,
+        string memory _description, 
+        string memory _occupation
+    ) public {
+        uint _vote = 0;
+        candidates[candidatesLength] = Candidate(
+            _name,
+            _image,
+            _description,
+            _occupation,
+            _vote
+        );
+        candidatesLength++;
+    }
+
+    function readCandidate(uint _index) public view returns (
+        string memory, 
+        string memory, 
+        string memory, 
+        string memory, 
+        uint
+    ) {
+        return (
+            candidates[_index].name, 
+            candidates[_index].image, 
+            candidates[_index].description, 
+            candidates[_index].occupation, 
+            candidates[_index].vote
+        );
+    }
+    
+    
+    function withdrawFunds(uint256 amount, address payable to)
+        external
+        onlyAdmin
+    {
+        _transferFunds(amount, to);
+    }
+
+    function _transferFunds(uint256 amount, address payable to) internal {
+        require(amount <= availableFunds, "not enough availableFunds");
+        availableFunds = availableFunds.sub(amount);
+        IERC20Token(cUsdTokenAddress).transfer(to, amount);
+    }
+    
+    
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "only admin");
+        _;
+    }
+    
+    function vote(uint _index, uint256 amount) public payable  {
+        require(
+          IERC20Token(cUsdTokenAddress).transferFrom(
+            msg.sender,
+            admin,
+            amount
+          ),
+          "Transfer failed."
+        );
+        availableFunds = availableFunds.add(amount);
+        candidates[_index].vote++;
+    }
+    
+    function getCandidatesLength() public view returns (uint) {
+        return (candidatesLength);
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Header from './components/Header';
-import { Jumbotron, Container } from 'reactstrap';
+import { Jumbotron, Container, Row, Col } from 'reactstrap';
 import React, { useEffect, useState } from 'react';
 import { getWeb3 } from './utils.js';
 import { newKitFromWeb3 } from "@celo/contractkit";
@@ -7,9 +7,10 @@ import BigNumber from "bignumber.js";
 import EVWP from './contracts/EVWP.abi.json';
 import Erc20Abi from './contracts/IERC20Token.abi.json'
 import { BeatLoader } from 'react-spinners';
+import CandidateCard from './components/CandidateCard';
 
 const ERC20_DECIMALS = 18
-const DaoContractAddress = "0x764354Ab0CDE955258aA7b6fE3C6718A868BA535"
+const evwpContractAddress = "0xEd3b8c053eB3A6FE7B63965F6698A51D1588f4BF"
 const cUSDContractAddress = "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1"
 
 
@@ -24,33 +25,29 @@ function App() {
   const [shares, setShares] = useState(undefined);
   const [proposals, setProposals] = useState([]);
   const [balance, setBalance] = useState(0);
-  const [isCelo, setIsCelo] = useState(false);
-  const [loading, setLoading] = useState(false);
 
 
   useEffect(() => {
     const init = async () => {
-      setLoading(true);
       const web3 = await getWeb3();
 
       kit = newKitFromWeb3(web3)
-      
+
       const accounts = await kit.web3.eth.getAccounts()
 
-      //console.log(accounts);
-      
       const contract = new kit.web3.eth.Contract(
-        EVWP, DaoContractAddress
+        EVWP, evwpContractAddress
       );
 
       const balance = await kit.getTotalBalance(accounts[0]);
       const USDBalance = balance.cUSD.shiftedBy(-ERC20_DECIMALS).toFixed(2);
-        
+
       const admin = await contract.methods
         .admin()
         .call();
-      
-      setLoading(false);
+
+      console.log(accounts[0]);
+
       setWeb3(web3);
       setAccounts(accounts);
       setContract(contract);
@@ -63,9 +60,29 @@ function App() {
     });
   }, []);
 
+
+  const isReady = () => {
+    return (
+      typeof contract !== 'undefined'
+      && typeof web3 !== 'undefined'
+      && typeof accounts !== 'undefined'
+      && typeof admin !== 'undefined'
+    );
+  }
+
+  useEffect(() => {
+    if (isReady()) {
+    }
+  }, [accounts, contract, web3, admin]);
+
+
+  if (!isReady()) {
+    return <BeatLoader />
+  }
+
   return (
     <div className="App">
-      <Header />
+      <Header balance={balance} />
       <Jumbotron>
         <Container>
           <h1 className="display-3">Easy voting with payment</h1>
@@ -73,7 +90,22 @@ function App() {
         </Container>
       </Jumbotron>
       <Container>
-        <BeatLoader/>
+        {accounts[0].toLowerCase() === admin.toLowerCase() ? (
+          <>
+            <Row>
+              <Col md="6" xs="6" sm="4"><h2>Withdraw funds</h2></Col>
+              <Col md="6" xs="6" sm="4"><h2>Add candidate</h2></Col>
+            </Row>
+            <hr />
+          </>
+        ) : null}
+        <Row>
+          <Col md="3" xs="6" sm="4"><CandidateCard /></Col>
+          <Col md="3" xs="6" sm="4"><CandidateCard /></Col>
+          <Col md="3" xs="6" sm="4"><CandidateCard /></Col>
+          <Col md="3" xs="6" sm="4"><CandidateCard /></Col>
+        </Row>
+
       </Container>
     </div>
   );
